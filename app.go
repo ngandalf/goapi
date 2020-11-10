5c970e39cde603984e11cad001c2ad53346fffff
nicolas.thomas <nicolas@dreaminformatique.com>
2020-11-11 00:05:21 +0100

    add contollers

--- app.go
+++ app.go
@@ -12,7 +12,8 @@ import (
     "encoding/json"
 
     "github.com/ngandalf/goapi/models"
-
+    "github.com/ngandalf/goapi/controllers"
+    
     "github.com/gorilla/mux"
     _ "github.com/lib/pq"
 )
@@ -41,29 +42,6 @@ func (a *App) Run(addr string) {
     log.Fatal(http.ListenAndServe(":8010", a.Router))
 }
 
-func (a *App) getProduct(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    id, err := strconv.Atoi(vars["id"])
-    if err != nil {
-        respondWithError(w, http.StatusBadRequest, "Invalid product ID")
-        return
-    }
-
-    p := models.Product{ID: id}
-    if err := p.GetProduct(a.DB); err != nil {
-        switch err {
-        case sql.ErrNoRows:
-            respondWithError(w, http.StatusNotFound, "Product not found")
-        default:
-            respondWithError(w, http.StatusInternalServerError, err.Error())
-        }
-        return
-    }
-
-    respondWithJSON(w, http.StatusOK, p)
-}
-
-
 func respondWithError(w http.ResponseWriter, code int, message string) {
     respondWithJSON(w, code, map[string]string{"error": message})
 }
@@ -75,87 +53,6 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
     w.WriteHeader(code)
     w.Write(response)
 }
-
-func (a *App) getProducts(w http.ResponseWriter, r *http.Request) {
-    count, _ := strconv.Atoi(r.FormValue("count"))
-    start, _ := strconv.Atoi(r.FormValue("start"))
-
-    if count > 10 || count < 1 {
-        count = 10
-    }
-    if start < 0 {
-        start = 0
-    }
-
-    products, err := models.GetProducts(a.DB, start, count)
-    if err != nil {
-        respondWithError(w, http.StatusInternalServerError, err.Error())
-        return
-    }
-
-    respondWithJSON(w, http.StatusOK, products)
-}
-
-func (a *App) createProduct(w http.ResponseWriter, r *http.Request) {
-    var p models.Product
-    decoder := json.NewDecoder(r.Body)
-    if err := decoder.Decode(&p); err != nil {
-        respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-        return
-    }
-    defer r.Body.Close()
-
-    if err := p.CreateProduct(a.DB); err != nil {
-        respondWithError(w, http.StatusInternalServerError, err.Error())
-        return
-    }
-
-    respondWithJSON(w, http.StatusCreated, p)
-}
-
-
-func (a *App) updateProduct(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    id, err := strconv.Atoi(vars["id"])
-    if err != nil {
-        respondWithError(w, http.StatusBadRequest, "Invalid product ID")
-        return
-    }
-
-    var p models.Product
-    decoder := json.NewDecoder(r.Body)
-    if err := decoder.Decode(&p); err != nil {
-        respondWithError(w, http.StatusBadRequest, "Invalid resquest payload")
-        return
-    }
-    defer r.Body.Close()
-    p.ID = id
-
-    if err := p.UpdateProduct(a.DB); err != nil {
-        respondWithError(w, http.StatusInternalServerError, err.Error())
-        return
-    }
-
-    respondWithJSON(w, http.StatusOK, p)
-}
-
-func (a *App) deleteProduct(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    id, err := strconv.Atoi(vars["id"])
-    if err != nil {
-        respondWithError(w, http.StatusBadRequest, "Invalid Product ID")
-        return
-    }
-
-    p := models.Product{ID: id}
-    if err := p.DeleteProduct(a.DB); err != nil {
-        respondWithError(w, http.StatusInternalServerError, err.Error())
-        return
-    }
-
-    respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
-}
-
 
 func (a *App) initializeRoutes() {
     a.Router.HandleFunc("/products", a.getProducts).Methods("GET")
